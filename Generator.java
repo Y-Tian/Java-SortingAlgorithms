d4fde140f71ff09fb01d0d383023a50250126865
Tony (Y.) Tian <t.tian4321@gmail.com>
2018-01-12 18:35:29 -0500

    Update Generator.java

--- Generator.java
+++ Generator.java
@@ -9,24 +9,20 @@ package sorting;
  * @author tony.tian
  */
 public class Generator {
-  
-    public Comparable[] generateArray(int size)
-    {
-        Comparable[] array = new Comparable[size];
-        for(int i = 0; i < size; i++)
-        {
-            array[i] = (int)(Math.random() * 2 * Integer.MAX_VALUE - Integer.MAX_VALUE);
-        }
-        return array;
-    }
-    
-    public Integer[] generateArrayInteger(int size)
-    {
-        Integer[] array = new Integer[size];
-        for(int i = 0; i < size; i++)
-        {
-            array[i] = (int)(Math.random() * 2 * Integer.MAX_VALUE - Integer.MAX_VALUE);
-        }
-        return array;
-    }
+
+ public Comparable[] generateArray(int size) {
+  Comparable[] array = new Comparable[size];
+  for (int i = 0; i < size; i++) {
+   array[i] = (int)(Math.random() * 2 * Integer.MAX_VALUE - Integer.MAX_VALUE);
+  }
+  return array;
+ }
+
+ public Integer[] generateArrayInteger(int size) {
+  Integer[] array = new Integer[size];
+  for (int i = 0; i < size; i++) {
+   array[i] = (int)(Math.random() * 2 * Integer.MAX_VALUE - Integer.MAX_VALUE);
+  }
+  return array;
+ }
 }

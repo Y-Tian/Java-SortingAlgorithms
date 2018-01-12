6f07cc35d4a7716bd242785726ce51a353ad3b8e
Tony (Y.) Tian <t.tian4321@gmail.com>
2018-01-12 18:36:12 -0500

    Update SortCompetitor.java

--- SortCompetitor.java
+++ SortCompetitor.java
@@ -9,10 +9,10 @@ package sorting;
  * @author tony.tian
  */
 public interface SortCompetitor {
-    public String getOwner();
-	public void sortn2(Comparable[] data);  
-	public void sortn2Alt(Comparable[] data);
-	public void sortnlgn(Comparable[] data);
-	public void sortnlgnAlt(Comparable[] data);
-	public void wildcard(Integer[] data);
+ public String getOwner();
+ public void sortn2(Comparable[] data);
+ public void sortn2Alt(Comparable[] data);
+ public void sortnlgn(Comparable[] data);
+ public void sortnlgnAlt(Comparable[] data);
+ public void wildcard(Integer[] data);
 }
